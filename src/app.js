35364d9188975ee72a42cbe7867c30473d646557
Carly Morris <thabai@hotmail.com>
2021-07-13 12:45:27 +0100

    created add, del, list, update with yargs

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const yargs = require("yargs");
+const command = process.argv[2];
+const fs = require("fs");
+
+const {
+    add,
+    del,
+    list,
+    update
+} = require("./Utils");
+let movieList = [];
+
+try {
+    let tempJson = fs.readFileSync('./films.json');
+    let tempNetflix = JSON.parse(tempJson)
+    movieList = tempNetflix;
+
+} catch (error) {
+    movieList = [];
+}
+
+const app = () => {
+    if (command === 'add') {
+        add(movieList, yargs.argv.movie, yargs.argv.year);
+    } else if (command === 'del') {
+        del(movieList, yargs.argv.movie, yargs.argv.year);
+    } else if (command === 'list') {
+        list(movieList);
+    } else if (command === 'update') {
+        update(movieList, yargs.argv.movie, yargs.argv.year, yargs.argv.movieNew, yargs.argv.yearNew);
+    }
+};
+
+app();
